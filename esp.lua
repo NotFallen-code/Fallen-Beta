e32c9bb88e136f6efb2c9cde78d3a08280f6a2ca
NotFallen-code <yusufy10000@gmail.com>
2025-12-31 23:40:22 +0000

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,20 +1,18 @@
 local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 local UserInputService = game:GetService("UserInputService")
-local RunService = game:GetService("RunService")
 
 local ESPEnabled = true
 
 local function createESP(player)
-	local char = player.Character
-	if char and char:WaitForChild("HumanoidRootPart") and not player:WaitForChild("PlayerGui"):FindFirstChild("ESP") then
+	if not player.Character or not player.Character:FindFirstChild("HumanoidRootPart") then return end
+	if player:FindFirstChild("PlayerGui") and not player.PlayerGui:FindFirstChild("ESP") then
 		local billboard = Instance.new("BillboardGui")
 		billboard.Name = "ESP"
-		billboard.Adornee = char.HumanoidRootPart
+		billboard.Adornee = player.Character.HumanoidRootPart
 		billboard.Size = UDim2.new(3, 0, 5, 0)
 		billboard.AlwaysOnTop = true
 		billboard.StudsOffset = Vector3.new(0, 0, 0)
-		billboard.Parent = player:WaitForChild("PlayerGui") 
+		billboard.Parent = player.PlayerGui
 
 		local frame = Instance.new("Frame")
 		frame.Size = UDim2.new(1, 0, 1, 0)
@@ -22,42 +20,47 @@ local function createESP(player)
 		frame.BackgroundTransparency = 0.5
 		frame.BorderSizePixel = 0
 		frame.Parent = billboard
-
 	end
 end
 
-
 local function removeESP(player)
-	if player:WaitForChild("PlayerGui"):FindFirstChild("ESP") then
-		player:WaitForChild("PlayerGui"):FindFirstChild("ESP"):Destroy()
+	if player:FindFirstChild("PlayerGui") and player.PlayerGui:FindFirstChild("ESP") then
+		player.PlayerGui.ESP:Destroy()
 	end
 end
 
 local function updateESPs()
-	for i, player in pairs(Players:GetPlayers()) do
-			if ESPEnabled == true then
-				createESP(player)
-			else
-				removeESP(player)
+	for _, player in pairs(Players:GetPlayers()) do
+		if ESPEnabled == true then
+			createESP(player)
+		else
+			removeESP(player)
 		end
 	end
 end
 
 Players.PlayerAdded:Connect(function(player)
-	player.CharacterAdded:Connect(function(Character)
+	player.CharacterAdded:Connect(function()
 		if ESPEnabled then
-			createESP(Character, player)
+			createESP(player)
 		end
 	end)
 end)
+
+for _, player in pairs(Players:GetPlayers()) do
+	player.CharacterAdded:Connect(function()
+		if ESPEnabled then
+			createESP(player)
+		end
+	end)
+	if ESPEnabled then
+		createESP(player)
+	end
+end
 
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
 	if not gameProcessed and input.KeyCode == Enum.KeyCode.R then
 		ESPEnabled = not ESPEnabled
 		updateESPs()
 	end
-end)
-
-RunService.RenderStepped:Connect(function()
-	updateESPs()
 end)
